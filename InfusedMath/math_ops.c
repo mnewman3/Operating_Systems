a04223fd06017478c6e84dc9cbe7a53e9394f6a2
mc4 <m.conleyy@gmail.com>
2015-04-21 16:53:00 -0400

    work on math ops

---
+++ InfusedMath/math_ops.c
@@ -0,0 +1,101 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* math operations */
+void factor(char *a, char *b);
+void fib(char *f);
+void add(char *a, char *b);
+void subtract(char *a, char *b);
+void multiply(char *a, char *b);
+void divide(char *a, char *b);
+void exponent(char *a, char *b);
+
+/* helpers */
+int isPrime(int n);
+
+void factor(char *a, char *b){
+
+}
+
+void fib(char * f){
+
+	double lim = atof(f);
+		printf("lim is: %f.\n", lim);
+
+	//check to see if lim is an integer
+	if(ceilf(lim) == lim){
+		printf("lim is: %f.\t", lim);
+		printf("Argument must be an integer.\n");
+	}
+
+
+
+}
+
+void add(char *a, char *b){
+
+	double x = atof(a);
+	double y = atof(b);
+
+	printf("%.2f\n", (x + y));
+
+}
+
+void subtract(char *a, char *b){
+
+	double x = atof(a);
+	double y = atof(b);
+
+	printf("%.2f\n", (x - y));
+}
+
+void multiply(char *a, char *b){
+
+	double x = atof(a);
+	double y = atof(b);
+
+	printf("%.2f\n", (x * y));
+
+}
+
+void divide(char *a, char *b){
+
+	double x = atof(a);
+	double y = atof(b);
+
+	if(b == 0){
+		printf("Cannot divide by 0.\n");
+		return;
+	}
+
+	printf("%.2f\n", (x / y));
+
+}
+
+void exponent(char *a, char *b){
+
+	double x = atof(a);
+	double y = atof(b);
+
+	printf("%.2f\n", pow(x,y));
+
+}
+
+/* returns 1 if argument is prime and 0 otherwise */
+int is_prime(int num){
+	if(num <= 1)
+		return 0;
+
+	if(num == 2)
+		return 1;
+
+	int lim = sqrt(num)+1;
+
+	for(int i = 2; i <= lim; i++){
+		if(num % i == 0){
+			return 0;
+		}
+	}
+	return 1;
+}
