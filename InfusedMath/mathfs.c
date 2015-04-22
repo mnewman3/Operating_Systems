15f36874dde4b9b7e1bddb2f2bd508a56e097320
Michael Newman <mnewmansnow@gmail.com>
2015-04-22 01:40:46 -0400

    changes to getattr: getting closer to done

--- InfusedMath/mathfs.c
+++ InfusedMath/mathfs.c
@@ -54,7 +54,7 @@ operation * is_valid_op(char * token){
 // FUSE function implementations.
 static int mathfs_getattr(const char *path, struct stat *stbuf)
 {
-
+    printf("%s-- In getattr, path: %s --%s\n", KMAG, path, KNRM);
     int ret = 0;
     char * token;
     char * commands[NUM_ARGS-1];   // args -1 b/c dont store command in here
@@ -67,10 +67,12 @@ static int mathfs_getattr(const char *path, struct stat *stbuf)
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
         return 0;
+    } else {
+        printf("%s-- getattr: not root dir --%s\n", KMAG, KNRM);
     }
 
     token = strtok((char*)path, "/");
-    
+
     // check if valid cmd from struct
     operation * op = is_valid_op(token);
 
@@ -78,22 +80,38 @@ static int mathfs_getattr(const char *path, struct stat *stbuf)
     if(op == NULL) {
         printf("%s-- Error: invalid operation dumbass --%s\n", KMAG, KNRM);
         return -ENOENT;
+    } else {
+        printf("%s-- getattr: valid operation --%s\n", KMAG, KNRM);
     }
 
+    // fill array with arguments
     int i;
     for(i = 0; i < (op->num_args); i++) {
         token = strtok(NULL, "/");
         commands[i] = token;
+        printf("%s-- arg%d : %s --%s\n", KMAG, i, token, KNRM);
     }
+
+    // check if path is operation folder
+    if(commands[0] == NULL) {
+        printf("%s-- Good shit, you're in the operation folder: %s --%s\n", KMAG, path, KNRM);
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 3;
+        return 0;
+    }
+
+    printf("%s-- getattr: finished filling args array --%s\n", KMAG, KNRM);
 
     // check if has more arguments than can take in
     token = strtok(NULL, "/");
     if(token != NULL){
         printf("%s-- Error: too many args --%s\n", KMAG, KNRM);
         return -ENOENT;
+    } else {
+        printf("%s-- getattr: valid number of args --%s\n", KMAG, KNRM);
     }
 
-    //check if doc
+    // check if doc
     if(strcmp(commands[0], "doc") == 0){
         if(commands[1] != NULL) {
             printf("%s-- Error: docs and too many args --%s\n", KMAG, KNRM);
@@ -104,7 +122,7 @@ static int mathfs_getattr(const char *path, struct stat *stbuf)
         stbuf->st_nlink = 1;
         stbuf->st_size = strlen(op->op_description);
         return 0;
-    }
+    } 
     
 
     return ret;
@@ -114,6 +132,7 @@ static int mathfs_getattr(const char *path, struct stat *stbuf)
 static int mathfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
             struct fuse_file_info *fi)
 {
+    printf("%s-- In readdir, path: %s --%s\n", KMAG, path, KNRM);
     (void) offset;
     (void) fi;
     return 0;
@@ -122,12 +141,14 @@ static int mathfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, o
 
 static int mathfs_open(const char *path, struct fuse_file_info *fi)
 {
+    printf("%s-- In open, path: %s --%s\n", KMAG, path, KNRM);
     return 0;
 
 }
 
 static int mathfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
+    printf("%s-- In read, path: %s --%s\n", KMAG, path, KNRM);
     (void) fi;
     return 0;
 
